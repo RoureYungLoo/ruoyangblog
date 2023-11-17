eb67f41572799e6df6290acda14b8e3dd7b0812c
RoureYungLoo <114283464+RoureYungLoo@users.noreply.github.com>
2023-11-17 14:36:47 +0800

    Create 2.14-条件分支switch.js

---
+++ 编程语言/JavaScript/js/JavaScript基础知识/2.14-条件分支switch.js
@@ -0,0 +1,82 @@
+// "switch" 语句
+/*
+switch(x) {
+  case 'value1':  // if (x === 'value1')
+    ...
+    [break]
+
+  case 'value2':  // if (x === 'value2')
+    ...
+    [break]
+
+  default:
+    ...
+    [break]
+}
+ */
+
+let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    console.log( 'Too small' );
+    break;
+  case 4:
+    console.log( 'Exactly!' );
+    break;
+  case 5:
+    console.log( 'Too big' );
+    break;
+  default:
+    console.log( "I don't know such values" );
+}
+
+// 如果没有 break，程序将不经过任何检查就会继续执行下一个 case。
+let a1 = 2 + 2;
+switch (a1) {
+  case 3:
+    console.log( 'Too small' );
+  case 4:
+    console.log( 'Exactly!' );
+  case 5:
+    console.log( 'Too big' );
+  default:
+    console.log( "I don't know such values" );
+}
+
+// case 分组
+let b = 3;
+switch (b) {
+  case 4:
+    console.log('Right!');
+    break;
+
+  case 3: // (*) 下面这两个 case 被分在一组
+  case 5:
+    console.log('Wrong!');
+    console.log("Why don't you take a math class?");
+    break;
+
+  default:
+    console.log('The result is strange. Really.');
+}
+
+// 严格相等
+// let arg = prompt("Enter a value?")
+let arg = '0'
+switch (arg) {
+  case '0':
+  case '1':
+    console.log( 'One or zero' );
+    break;
+
+  case '2':
+    console.log( 'Two' );
+    break;
+
+  case 3:
+    console.log( 'Never executes!' );
+    break;
+  default:
+    console.log( 'An unknown value' )
+}
