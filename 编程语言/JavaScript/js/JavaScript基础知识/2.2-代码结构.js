ceb392415d3afdbcbdf7a71ec5ce275470585d1e
RoureYungLoo <114283464+RoureYungLoo@users.noreply.github.com>
2023-11-17 14:30:04 +0800

    Create 2.2-代码结构.js

---
+++ 编程语言/JavaScript/js/JavaScript基础知识/2.2-代码结构.js
@@ -0,0 +1,22 @@
+// alert('Hello'); alert('World');
+
+alert("Hello");
+
+[1, 2].forEach(alert);
+
+// https://cloud.tencent.com/developer/article/1651333
+
+//单行注释
+
+/*
+多行注释
+*/
+
+/**
+ * 函数注释
+ *
+ */
+
+//注释不支持嵌套
+
+// JSDoc 
